6b49e76286629ced335dd1d1eee1d683e673ff78
Adam Golota <golota.adam@gmail.com>
2018-10-05 00:02:04 +0300

    gitignore

--- src/config.py
+++ src/config.py
@@ -1,14 +1,14 @@
-debug = True
+debug = False
 idle = True
 
 token = '688585334:AAGN0xWW-mluRfTICRaZ02JkNpiP-trdIQM'
 mail = 'kpi.tef.ta71@gmail.com'
 password = 'qvpkonjbrpldalak'
+host = 'imap.gmail.com'
 
 if debug :
     chat_id = '-1001182500001'
     mail = 'kpi.tef.ta71@gmail.com'
     password = 'qvpkonjbrpldalak'
-    host = 'imap.gmail.com'
 else :
     chat_id = '@tef_ta71'
